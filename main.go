f8fe8baf6a2e99771d444d70b03c829bdc44a36d
James Clarke <hello@jclarke.dev>
2024-01-03 19:26:26 +0000

    Improve test script

--- main.go
+++ main.go
@@ -2,18 +2,22 @@ package main
 
 import (
 	"chord/chord"
+	"fmt"
 )
 
 func main() {
 	lead := chord.CreateNode(1)
 	lead.Start()
 
-	s := make([]*chord.Node, 10)
-	for i := 1; i < 10; i++ {
-		s[i] = chord.CreateNode(chord.Id(i) + 1)
+	n := 32
+	s := make([]*chord.Node, n)
+	for i := 1; i < n; i++ {
+		s[i] = chord.CreateNode(chord.Id(1 << i))
 		s[i].Join(lead)
 		s[i].Start()
 	}
+
+	fmt.Println("set up...starting")
 
 	for {
 	}

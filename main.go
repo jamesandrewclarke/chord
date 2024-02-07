a673d1796d65e18223c233a03732851bfb0715b2
James Clarke <hello@jclarke.dev>
2024-02-07 15:57:29 +0000

    Updates to entrypoint

--- main.go
+++ main.go
@@ -9,21 +9,17 @@ func main() {
 	lead := chord.CreateNode(0)
 	lead.Start()
 
-	n := 32
+	n := 8
 	s := make([]*chord.Node, n)
 
-	s[0] = chord.CreateNode(0)
-	s[0].Start()
-
 	for i := 1; i < n; i++ {
-		// s[i] = chord.CreateNode(chord.Id(1 << i))
-		s[i] = chord.CreateNode(chord.Id(i))
+		s[i] = chord.CreateNode(chord.Id(1 << i))
+		// s[i] = chord.CreateNode(chord.Id(i))
 		s[i].Join(lead)
 		s[i].Start()
 	}
 
 	fmt.Println("set up...starting")
 
-	for {
-	}
+	select {}
 }

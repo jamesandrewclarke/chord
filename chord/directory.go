7d2e23d10f47fbd0771a1e318e2c96f4d0172360
James Clarke <hello@jclarke.dev>
2024-03-04 20:20:34 +0000

    Rename 'peers' to 'directory'

--- chord/directory.go
+++ chord/directory.go
@@ -2,21 +2,10 @@ package chord
 
 import "fmt"
 
-var peers map[Id]node
-
 var peerAddresses map[Id]string
 
 func init() {
 	peerAddresses = make(map[Id]string)
-}
-
-// getPeer returns a node given an identifier
-func getPeer(id Id) (node, error) {
-	if peer, ok := peers[id]; ok {
-		return peer, nil
-	}
-
-	return nil, fmt.Errorf("unable to locate peer %v in cache", id)
 }
 
 func SetPeerAddress(id Id, addr string) {

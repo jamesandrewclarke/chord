ff9885b3e0a3dab69b6aa972130ef28cef672d59
James Clarke <hello@jclarke.dev>
2024-03-11 16:47:49 +0000

    Add locks for directory

--- chord/directory.go
+++ chord/directory.go
@@ -1,6 +1,13 @@
 package chord
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net"
+	"sync"
+)
+
+var mu sync.Mutex
 
 var peerAddresses map[Id]string
 
@@ -9,10 +16,23 @@ func init() {
 }
 
 func SetPeerAddress(id Id, addr string) {
+	_, _, err := net.SplitHostPort(addr)
+	if err != nil {
+		log.Printf("Invalid address for %v: `%v`", id, addr)
+		return
+	}
+
+	mu.Lock()
+	defer mu.Unlock()
+	if currAddr, ok := peerAddresses[id]; ok && currAddr != addr {
+		log.Printf("overwriting address for %d\n from %v to %v", id, currAddr, addr)
+	}
 	peerAddresses[id] = addr
 }
 
 func getPeerAddress(id Id) (string, error) {
+	mu.Lock()
+	defer mu.Unlock()
 	if addr, ok := peerAddresses[id]; ok {
 		return addr, nil
 	}

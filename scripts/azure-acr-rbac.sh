d825a869e30129941fe8d3b863a214f14d7d73ef
James Clarke <hello@jclarke.dev>
2024-04-05 20:44:38 +0100

    Fix ACR credentials script

--- scripts/azure-acr-rbac.sh
+++ scripts/azure-acr-rbac.sh
@@ -2,16 +2,18 @@
 # Gets credentials to use as secrets for GitHub Actions
 # Allows pushing and pulling from the ACR repository
 
+set -ex
+
 ACR_NAME=chord
 SERVICE_PRINCIPAL_NAME=gh-actions
-SUBSCRIPTION=bc56aec8-74ab-408c-bf65-f69efd5d2446
+SUBSCRIPTION=66f773f7-ab57-4a46-9c43-c11a2caf7c9d
 
 # Obtain the full registry ID
 ACR_REGISTRY_ID=$(az acr show --name $ACR_NAME --query "id" --output tsv --subscription $SUBSCRIPTION)
 echo "Registry ID: $ACR_REGISTRY_ID"
 
-PASSWORD=$(az ad sp create-for-rbac --name $SERVICE_PRINCIPAL_NAME --scopes $ACR_REGISTRY_ID --role acrpull --query "password" --output tsv --subscription $SUBSCRIPTION)
-USER_NAME=$(az ad sp list --display-name $SERVICE_PRINCIPAL_NAME --query "[].appId" --output tsv --subscription $SUBSCRIPTION)
+PASSWORD=$(az ad sp create-for-rbac --name $SERVICE_PRINCIPAL_NAME --scopes $ACR_REGISTRY_ID --role contributor --query "password" --output tsv)
+USER_NAME=$(az ad sp list --display-name $SERVICE_PRINCIPAL_NAME --query "[].appId" --output tsv)
 
 # Output the service principal's credentials; use these in your services and
 # applications to authenticate to the container registry.
